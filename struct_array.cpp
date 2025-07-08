f1481fda1bc5b1a1cf6aa5c9853a5b40775598c8
restuchan <restu.chan188@gmail.com>
2025-07-09 01:07:34 +0700

    main function

--- struct_array.cpp
+++ struct_array.cpp
@@ -8,3 +8,29 @@ struct Hero
     string tipe;
 };
 
+int main()
+{
+    Hero arhero[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "hero ke- " << i + 1 << endl;
+        cout << "masukkan nama hero = ";
+        cin >> arhero[i].nama;
+        cout << "masukkan jenis role = ";
+        cin >> arhero[i].role;
+        cout << "masukkan tipe hero = ";
+        cin >> arhero[i].tipe;
+        cout << endl;
+     
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "hero ke- " << i + 1 << endl;
+        cout << "nama hero = " << arhero[i].nama << endl;
+        cout << "jenis role hero = " << arhero[i].role << endl;
+        cout << "tipe hero = " << arhero[i].tipe << endl;
+    }
+    cout << endl;
+    cout << "tampilkan hero" << endl;
+    cout << endl;
+}
