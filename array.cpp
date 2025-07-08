67af6d1f48d6b50d44265a52a81a6e804ff99555
restuchan <restu.chan188@gmail.com>
2025-07-09 00:58:06 +0700

    stage all

--- array.cpp
+++ array.cpp
@@ -9,4 +9,28 @@ char grade[5];
 
 int main()
 {
-   
+    cout << "nama matkul : " << matkul [0] << endl;
+    matkul[0] = "logika teknik pemrograman";
+    cout << "nama matkul : " << matkul [0] << endl;
+
+    for(int i = 0; i < 2; i++)
+    {
+        cout << "nilai akhir ke- " << i+1 << " = " << nilaiAkhir[i] << endl;
+    }
+
+    cout << "input grade" << endl;
+    for(int i = 0 ; i < 5; i++)
+    {
+        cout << "masukkan grade ";
+        cin >> grade[i];
+    }
+
+    cout << endl;
+    cout << "tampilkan grade" << endl;
+    cout << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "grade = " << grade[i] << endl;
+    }
+}
