0d094ab87a773b6d2c94cf906ac940d81e59d2cf
restuchan <restu.chan188@gmail.com>
2025-07-09 01:03:46 +0700

    main function

--- nested_struct.cpp
+++ nested_struct.cpp
@@ -16,3 +16,41 @@ struct Hero
     skill skill;
 };
 
+int main()
+{
+    Hero arhero[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "hero ke- " << i + 1 << endl;
+        cout << "masukkan nama hero = ";
+        getline(cin , arhero[i].nama);
+        cout << "masukkan jenis role = ";
+        getline(cin , arhero[i].role);
+        cout << "masukkan tipe hero = ";
+        cin >> arhero[i].tipe;
+        cout << endl;
+        cin.ignore();
+        cout << "masukkan skill 1 = ";
+        getline(cin , arhero[i].skill.skill1);
+        cout << "masukkan skill 2 = ";
+        getline(cin , arhero[i].skill.skill2);
+        cout << "masukkan skill 3 = ";
+        getline(cin , arhero[i].skill.skill3);
+    }
+
+    cout << endl;
+    cout << "tampilkan hero" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "hero ke- " << i + 1 << endl;
+        cout << "nama hero = " << arhero[i].nama << endl;
+        cout << "jenis role hero = " << arhero[i].role << endl;
+        cout << "tipe hero = " << arhero[i].tipe << endl;
+
+        cout << "skill 1 = " << arhero[i].skill.skill1 << endl;
+        cout << "skill 2 = " << arhero[i].skill.skill2 << endl;
+        cout << "skill 3 = " << arhero[i].skill.skill3 << endl;
+    }
+}
